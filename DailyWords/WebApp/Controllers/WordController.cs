3d81c3f248c85b3f519a4d80639734672bc40169
Vinicius Lucena <vilucenasantos@gmail.com>
2022-02-09 16:10:50 -0300

    commit inicial

---
+++ DailyWords/WebApp/Controllers/WordController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WordController : ControllerBase
+    {
+        private readonly ILogger<WordController> _logger;
+
+        public WordController(ILogger<WordController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Word Get()
+        {
+            return new Word { Message = "Test" };
+        }
+    }
+}
---
+++ DailyWords/WebApp/Word.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace WebApp
+{
+    public class Word
+    {
+        public string Message { get; set; }
+    }
+}
